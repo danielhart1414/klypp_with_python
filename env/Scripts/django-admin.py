49f9b7802248054bfceec3fcf294cf5114568cb1
Daniel Hart <daniel_hart@live.com>
2017-11-13 19:24:57 -0500

    Updated Readme; got home page up and running

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\daniel\documents\business\codelouisville\webserver\klypp_with_python\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
